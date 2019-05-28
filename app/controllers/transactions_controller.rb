47b2002dd094e2cf3cb1f0d7400a4f71d05ff760
daniel.samosir <daniel.samosir@aux.go-jek.com>
2019-05-28 23:21:11 +0700

    Add spec for creating transaction return 404 response when ticket id is not found

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,11 +1,16 @@
 class TransactionsController < ApplicationController
   def create
+    ticket = Ticket.find(transaction_params['tickets'][0]['ticket_id'])
     @transaction = Transaction.new(transaction_params)
     if @transaction.save
       render json: @transaction, status: 200
     else
       render json: { message: "Validation failed", errors: @transaction.errors }, status: 400
     end
+    rescue ActiveRecord::RecordNotFound => e
+      render json: {
+        error: e.to_s
+      }, status: :not_found
   end
 
   private
