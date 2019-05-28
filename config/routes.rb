16ad5881a6460438f3390fd9fd8c37ed8d4436f8
daniel.samosir <daniel.samosir@aux.go-jek.com>
2019-05-28 21:10:35 +0700

    Add route for ticket creation

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   post '/location/create', to: 'locations#create'
   post '/event/create', to: 'events#create'
+  post '/event/ticket/create', to: 'tickets#create'
 end
