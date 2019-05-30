d62f44cd31aceacf15daf4c61803952b581e99bb
daniel.samosir <daniel.samosir@aux.go-jek.com>
2019-05-30 14:19:16 +0700

    Add spec for creating user return 400 response when attribute is nil

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,8 +1,11 @@
 class CustomersController < ApplicationController
   def create
     @customer = Customer.new(customer_params)
-    @customer.save
-    render json: @customer, status: 200
+    if @customer.save
+      render json: @customer, status: 200
+    else
+      render json: { message: "Validation failed", errors: @customer.errors }, status: 400
+    end
   end
 
   private
--- spec/controllers/customers_controller_spec.rb
+++ spec/controllers/customers_controller_spec.rb
@@ -9,5 +9,13 @@ RSpec.describe CustomersController, type: :controller do
         expect(subject).to have_http_status(:ok)
       end
     end
+
+    context 'When create a customer with nil attributes' do
+      subject { post :create, params: { customer: { name: nil, email:'daniel@daniel.com', phone: '123' } } }
+
+      it 'returns a 400 OK status' do
+        expect(subject).to have_http_status(400)
+      end
+    end
   end
 end
