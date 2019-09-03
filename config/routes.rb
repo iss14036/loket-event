1cb04d707833ea597474a2b895f0cc7390e902a0
Daniel Hara Tua Samosir <danielharatuasamosir@Daniels-MacBook-Pro.local>
2019-09-03 13:59:11 +0700

    Add route and aithorize_request in application controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  resources :users, param: :_username
+  post '/auth/login', to: 'authentication#login'
+  get '/*a', to: 'application#not_found'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   post '/location/create', to: 'locations#create'
   post '/event/create', to: 'events#create'
